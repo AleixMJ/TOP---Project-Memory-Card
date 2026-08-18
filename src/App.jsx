ed3c16ff7421257f8d29b2fa0e6d1d466b10d59b
AleixMJ <aleixtechIT@gmail.com>
2026-08-18 20:46:31 +0200

    feat(ui) added main text for the header

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,25 @@ function App() {
 
 
   return (
+    <>
+      <div id='header'>
+        <div id='header-left-section'>
+          <h1>Kodamara Memory Game</h1>
+          <p>get points by clicking on an image but don't click on any more than once!</p>
+        </div>
+        <div id='header-right-section'>
+          <span>
+            Score: 
+          </span>
+          <span>
+            Best score: 
+          </span>
+        </div>
+      </div>
 
-    <div className='cards-container'>
+
+
+      <div className='cards-container'>
       {cardsData.map((card) => (
         <div className='card' key={card.id}>
           <img src={card.image} alt={card.name}></img>
@@ -15,6 +32,8 @@ function App() {
       ))}
 
     </div>
+    </>
+
     
     );
 
