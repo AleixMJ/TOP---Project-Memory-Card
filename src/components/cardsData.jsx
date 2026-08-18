4394197f2376254413268f207d9abc4a6b7b7a37
AleixMJ <aleixtechIT@gmail.com>
2026-08-18 19:32:16 +0200

    style: set up 7-column card grid and page background theme

---
+++ src/components/cardsData.jsx
@@ -0,0 +1,31 @@
+import kodamara12610 from '../assets/Kodamara-12610.webp';
+import kodamara18282 from '../assets/Kodamara-18282.webp';
+import kodamara23159 from '../assets/Kodamara-23159.webp';
+import kodamara45838 from '../assets/Kodamara-45838.webp';
+import kodamara48041 from '../assets/Kodamara-48041.webp';
+import kodamara50284 from '../assets/Kodamara-50284.webp';
+import kodamara51918 from '../assets/Kodamara-51918.webp';
+import kodamara64067 from '../assets/Kodamara-64067.webp';
+import kodamara65114 from '../assets/Kodamara-65114.webp';
+import kodamara74797 from '../assets/Kodamara-74797.webp';
+import kodamara88562 from '../assets/Kodamara-88562.webp';
+import kodamara91201 from '../assets/Kodamara-91201.webp';
+import kodamara97765 from '../assets/Kodamara-97765.webp';
+import kodamara99525 from '../assets/Kodamara-99525.webp';
+
+export const cardsData = [
+  { id: '12610', name: 'Spore Cyclops', image: kodamara12610, isClicked: false },
+  { id: '18282', name: 'Verdant Triclops', image: kodamara18282, isClicked: false },
+  { id: '23159', name: 'Ember Jester', image: kodamara23159, isClicked: false },
+  { id: '45838', name: 'Vine Weaver', image: kodamara45838, isClicked: false },
+  { id: '48041', name: 'Tide Crab', image: kodamara48041, isClicked: false },
+  { id: '50284', name: 'Infernal Imp', image: kodamara50284, isClicked: false },
+  { id: '51918', name: 'Crystal Sprite', image: kodamara51918, isClicked: false },
+  { id: '64067', name: 'Crimson Phantom', image: kodamara64067, isClicked: false },
+  { id: '65114', name: 'Ooze Blob', image: kodamara65114, isClicked: false },
+  { id: '74797', name: 'Void Shaman', image: kodamara74797, isClicked: false },
+  { id: '88562', name: 'Sun Sovereign', image: kodamara88562, isClicked: false },
+  { id: '91201', name: 'Astral Jester', image: kodamara91201, isClicked: false },
+  { id: '97765', name: 'Gold Oculus', image: kodamara97765, isClicked: false },
+  { id: '99525', name: 'Deepwater Beast', image: kodamara99525, isClicked: false },
+];
